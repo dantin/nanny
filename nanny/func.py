a71e761b15645de776e9926a40984fa4958b4216
dantin <chengjie.ding@gmail.com>
2021-05-19 10:27:22 +0800

    fix unit test.

--- nanny/func.py
+++ nanny/func.py
@@ -5,10 +5,10 @@ import datetime
 YYYY_MM_DD = '%Y-%m-%d'
 
 
-def duration_in_day(since=datetime.datetime.today(), days=7, time_format=YYYY_MM_DD):
+def duration_in_day(start=datetime.datetime.today(), days=7, time_format=YYYY_MM_DD):
     """duration_in_day returns days"""
-    end = since + datetime.timedelta(days=days)
-    d, s = since, []
+    end = start + datetime.timedelta(days=days)
+    d, s = start, []
     while d < end:
         s.append(d.strftime(time_format))
         d += datetime.timedelta(days=1)
